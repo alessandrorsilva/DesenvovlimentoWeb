347c3f619303b8ef2dc2c1063191d7c825c90b36
Alessandro Rodrigues da Silva <alessandrosilva.ti@gmail.com>
2021-10-03 18:00:34 -0300

    Exercicio 01

---
+++ exercicios/exercicio01/src/exercicio01.java
@@ -0,0 +1,28 @@
+public class exercicio01 {
+
+    public static void main(String[] args){
+
+        //Escreva um programa que declare, inicialize e imprima as seguintes variáveis:
+        //• Inteiro i de 32 bits com valor 1;
+        //• Inteiro j de 64 bits com valor 2;
+        //• Ponto flutuante p de 32 bits com valor 20.0;
+        //• Ponto flutuante q de 64 bits com valor 30.0;
+        //• Booleano b com valor verdadeiro;
+        //• Caractere c com valor ‘k’;
+
+        int i = 1;
+        float j = 2;
+        float p = 20.0F;
+        double q = 30.0;
+        boolean b = true;
+        char c = 'K';
+
+        System.out.println("Inteiro i de 32 bits com valor 1: " + i);
+        System.out.println("Inteiro j de 64 bits com valor 2: " + j);
+        System.out.println("Ponto flutuante p de 32 bits com valor 20.0: " + p);
+        System.out.println("Ponto flutuante q de 64 bits com valor 30.0: " + q);
+        System.out.println("Booleano b com valor verdadeiro: " + b);
+        System.out.println("Caractere c com valor ‘k’ : " + c);
+
+    }
+}

3b02ce370fee50e1f8e1b910b0c5b6515a6e5651
Alessandro Rodrigues da Silva <alessandrosilva.ti@gmail.com>
2021-10-03 17:33:58 -0300

    Entrada de dados

---
+++ exercicios/entradadedados/src/entradadedados.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class entradadedados {
+
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Qual o seu nome?");
+        String name =scanner.next();
+        System.out.println("Ah, entendi... O seu nome é: " + name);
+        System.out.println("Você vem sempre aqui??");
+
+    }
+}
